d8dc72a36c5ae53743320fc60138cb444ae98d72
Jared Scott <104541761+JaredScott66@users.noreply.github.com>
2025-03-23 14:02:11 -0500

    ahhh

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const shipsRoute = require('./shipRegistry');
 
 router.use('/', require('./swagger'));
 
-// router.get('/', (req, res) => {
-//     //#swagger.tags=['Hello world']
-//     res.send('Hello World');
-// });
+router.get('/', (req, res) => {
+    //#swagger.tags=['Hello world']
+    res.send('Hello World');
+});
 
 router.use('/users', usersRoute);
 router.use('/ship-in', shipsRoute);
